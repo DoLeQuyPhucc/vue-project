4627bd6dbb3cf0a594f3d671dd804cdec6a78a3a
Phuc <phucdlq@gmail.com>
2025-05-29 23:49:51 +0700

    add: fix css app

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,5 @@
 <template>
-  <div class="app">
-    <router-view />
-  </div>
+  <router-view />
 </template>
 
-<script setup>
-</script>
-
-<style>
-.app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<script setup></script>
